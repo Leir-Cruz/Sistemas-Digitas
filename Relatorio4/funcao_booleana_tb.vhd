3d33c376fbabc200ee51ae2b97d0fcb8fbeefec9
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2022-03-03 23:37:40 -0300

    :bug: corrigindo testes
    :sparkles: rodando compilador

--- Relatorio4/funcao_booleana_tb.vhd
+++ Relatorio4/funcao_booleana_tb.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity funcao_booleana_tb is end;
 
-architecture funcao_booleana_arch of funcao_booleana_arch_tb is
+architecture funcao_booleana_arch of funcao_booleana_tb is
     component funcao_booleana is
         port (
             A: in std_logic;
@@ -15,17 +15,17 @@ architecture funcao_booleana_arch of funcao_booleana_arch_tb is
     end component;
     
     --quanto mais significativo maior o tempo
-    signal entracce: in std_logic_vector (2 downto 0) := "000";
+    signal entrance: std_logic_vector (2 downto 0) := "000";
 
     constant time_0: time := 2ns;
     constant time_1: time := 4ns;
     constant time_2: time := 8ns;
 
     begin
-        funcao_booleana_1: funcao_booleana port ( A => entrace(2), B => entrace(1), C => entrace(0), X => open, Y => open );
+        funcao_booleana_1: funcao_booleana port map ( A => entrance(2), B => entrance(1), C => entrance(0), X => open, Y => open );
 
         entrance(2) <= not entrance(2) after time_2/2;
         entrance(1) <= not entrance(1) after time_1/2;
         entrance(0) <= not entrance(0) after time_0/2;
 
-end funcao_booleana_arch
+end funcao_booleana_arch;
