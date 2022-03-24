81415740052049d965a95bebfc5d9d9a9fafc1f6
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2022-03-24 11:40:29 -0300

    :rotating_light: corringindo erros apresentados no compilador
    :package: adicionando arquivos de compilador

--- Relatorio6/flipflopjk.vhd
+++ Relatorio6/flipflopjk.vhd
@@ -8,22 +8,23 @@ entity flipflopJK is
     );
 end flipflopJK;
 
-architecture flipflopJK_arch of fliflopJK is
+architecture flipflopJK_arch of flipflopJK is
     signal Qbuf: std_logic := '0';
     signal JK: std_logic_vector (1 downto 0);
     begin
         JK <= J & K;
         process(clk, clr, pr)
         begin
-            if pr == '1' then Qbuf <= '1';
-            elsif pr == '0' and clr == '0' then Qbuf <= '0';
+            if pr = '1' then Qbuf <= '1';
+            elsif pr = '0' and clr = '0' then Qbuf <= '0';
             elsif rising_edge(clk) then
                 case JK is
-                    when '00' then => Qbuf <= Qbuf;  
-                    when '01' then => Qbuf <= '0';   
-                    when '10' then => Qbuf <= '1';
-                    when '11' then => Qbuf <= not(Qbuf);
-                end case
-            end if 
+                    when "01" => Qbuf <= '0';   
+                    when "10" => Qbuf <= '1';
+                    when "11" => Qbuf <= not(Qbuf);
+                    when others => Qbuf <= Qbuf;  
+                end case;
+            end if;
             Q <= Qbuf;
+            end process;
 end flipflopJK_arch;
