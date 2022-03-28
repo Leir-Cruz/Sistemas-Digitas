fd36aa24c1fe3dfa6e003d1fcfa0e0d2f0a9c042
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2022-03-27 22:25:38 -0300

    :bug: declaracao variavel estado

--- Relatorio7/refrigerante.vhd
+++ Relatorio7/refrigerante.vhd
@@ -12,7 +12,7 @@ end maquinaEstados;
 
 architecture maquinaEstados_arch of maquinaEstados is
 
-    type estado is (init, 25c, 50c, 75c, 100c, 125c, d25, d50, d75);
+    type estado is (init, c25, c50, c75, c100, c125, d25, d50, d75);
     signal currentState, nextState : estado;
 
 begin
@@ -31,11 +31,68 @@ begin
         case currentState is
             when init =>
                 Y <= "000";
-                if(A = "01") then nextState <= 25c;
-                elsif(A = "10") then nextState <= 50c;
+                if(A = "01") then nextState <= c25;
+                elsif(A = "10") then nextState <= c50;
                 elsif(A = "11") then nextState <= init;
                 else nextState <= init;
                 end if;
+            when c25 =>
+                Y <= "000";
+                if(A = "01") then nextState <= c50;
+                elsif(A = "10") then nextState <= c75;
+                elsif(A = "11") then nextState <= d25;
+                else nextState <= c25;
+                end if;
+            when c50 =>
+                Y <= "000";
+                if(A = "01") then nextState <= c75;
+                elsif(A = "10") then nextState <= c100;
+                elsif(A = "11") then nextState <= d50;
+                else nextState <= c50;
+                end if;
+            when c75 =>
+                Y <= "000";
+                if(A = "01") then nextState <= c100;
+                elsif(A = "10") then nextState <= c125;
+                elsif(A = "11") then nextState <= d75;
+                else nextState <= c75;
+                end if;
+            when c100 =>
+                Y <= "100";
+                if(A = "01") then nextState <= c25;
+                elsif(A = "10") then nextState <= c50;
+                elsif(A = "11") then nextState <= init;
+                else nextState <= init;
+                end if;
+            when c125 =>
+                Y <= "110";
+                if(A = "01") then nextState <= c25;
+                elsif(A = "10") then nextState <= c50;
+                elsif(A = "11") then nextState <= init;
+                else nextState <= init;
+                end if;
+            when d25 =>
+                Y <= "010";
+                if(A = "01") then nextState <= c25;
+                elsif(A = "10") then nextState <= c50;
+                elsif(A = "11") then nextState <= init;
+                else nextState <= init;
+                end if;
+            when d50 =>
+                Y <= "001";
+                if(A = "01") then nextState <= c25;
+                elsif(A = "10") then nextState <= c50;
+                elsif(A = "11") then nextState <= init;
+                else nextState <= init;
+                end if;
+            when d75 =>
+                Y <= "011";
+                if(A = "01") then nextState <= c25;
+                elsif(A = "10") then nextState <= c50;
+                elsif(A = "11") then nextState <= init;
+                else nextState <= init;
+                end if;
+            when others => nextState <= nextState;
         end case;
     end process comb_proc;
 
